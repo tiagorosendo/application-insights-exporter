33d80141531126eeb8750652e9502a6895e37bbc
Tiago Rosendo <trosendo.dev@gmail.com>
2018-11-22 02:13:01 -0200

    passing queries by env-variables

--- application-insights-exporter/index.js
+++ application-insights-exporter/index.js
@@ -5,42 +5,45 @@ const appId = process.env.APPID
 const appKey = process.env.APPKEY
 
 const appInsightsQueryUrl = applicationInsightsRootUrl + '/v1/apps/' + appId + '/query'
-const dependenciesDataSetUrl = process.env.DATASETURL
+
+const queries = JSON.parse(process.env.QUERIES)
 
 module.exports = async function (context) {
-  const requestCount = {
-    'query': 'dependencies | where timestamp > ago(5m) | where operation_Name == "POST Split/Post" | summarize avgRequestDuration=avg(duration) by name, bin(timestamp, 1m)'
-  }
-
-  const dataSet = await fetch(appInsightsQueryUrl, {
-    method: 'post',
-    body: JSON.stringify(requestCount),
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': appKey
+  for (const item of queries) {
+    const requestBody = {
+      'query': item.query
     }
-  })
-    .then(res => res.json())
-    .then(table => table.tables[0])
-    .then(table => {
-      const columns = table.columns
-      const rows = table.rows
-      return {
-        columns,
-        rows
+
+    const dataSet = await fetch(appInsightsQueryUrl, {
+      method: 'post',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': appKey
       }
     })
-    .then(table => table.rows.map(elem => elem.reduce((obj, item, index) => {
-      obj[table.columns[index].name] = item
-      return obj
-    }, {})))
-    .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(table => table.tables[0])
+      .then(table => {
+        const columns = table.columns
+        const rows = table.rows
+        return {
+          columns,
+          rows
+        }
+      })
+      .then(table => table.rows.map(elem => elem.reduce((obj, item, index) => {
+        obj[table.columns[index].name] = item
+        return obj
+      }, {})))
+      .catch(err => console.log(err))
 
-  await fetch(dependenciesDataSetUrl, {
-    method: 'post',
-    body: JSON.stringify(dataSet)
-  }).then(_ => context.log('The dataSet was sended to PowerBI'))
-    .catch(err => console.log(err))
+    await fetch(item.dataSetUrl, {
+      method: 'post',
+      body: JSON.stringify(dataSet)
+    }).then(_ => context.log('The dataSet was sended to PowerBI'))
+      .catch(err => console.log(err))
+  }
 
   context.log('Functions is over')
 }
